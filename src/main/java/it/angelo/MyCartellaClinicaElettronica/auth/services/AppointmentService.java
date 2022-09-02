c3c1442955e3dd767818ab51db6ef3512e2062ea
Davide Fazio <drumprog@gmail.com>
2022-09-02 18:29:38 +0200

    Added log functions to AppointmentService.java

--- src/main/java/it/angelo/MyCartellaClinicaElettronica/auth/services/AppointmentService.java
+++ src/main/java/it/angelo/MyCartellaClinicaElettronica/auth/services/AppointmentService.java
@@ -4,6 +4,7 @@ import it.angelo.MyCartellaClinicaElettronica.appointment.entities.Appointment;
 import it.angelo.MyCartellaClinicaElettronica.appointment.entities.AppointmentDTO;
 import it.angelo.MyCartellaClinicaElettronica.appointment.repositories.AppointmentRepository;
 import it.angelo.MyCartellaClinicaElettronica.auth.controllers.PasswordRestoreController;
+import it.angelo.MyCartellaClinicaElettronica.auth.controllers.SignupController;
 import it.angelo.MyCartellaClinicaElettronica.user.entities.User;
 import it.angelo.MyCartellaClinicaElettronica.user.repositories.UserRepository;
 import it.angelo.MyCartellaClinicaElettronica.user.utils.Roles;
@@ -38,6 +39,7 @@ public class AppointmentService {
      */
     public Appointment save(AppointmentDTO appointmentInput)throws Exception{
         // rappresenta un utente autenticato, la gestione è demandata a JwtTokenFilter class
+        logger.info("Public method 'save' method called at "+ AppointmentService.class +" at line#" + lineGetter);
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Appointment appointment = new Appointment();
         appointment.setCreatedAt(LocalDateTime.now());
@@ -56,8 +58,12 @@ public class AppointmentService {
 
         //check for patient
         if(appointmentInput.getPatient() == null) throw new Exception("Patient not found");
+        logger.error("if statement from public Appointment 'save' inside :"+ AppointmentService.class +" at line#" +
+                lineGetter + "- Error : 'Patient is null");
         Optional<User> patient = userRepository.findById(appointmentInput.getPatient());
         if(!patient.isPresent() || !Roles.hasRole(patient.get(), Roles.PATIENT)) throw new Exception("Patient not found");
+        logger.error("if statement from public Appointment 'save' inside "+ AppointmentService.class +" at line#" +
+                lineGetter + "- Error : 'Patient not found");
 
         appointment.setPatient(patient.get());
 
@@ -65,8 +71,11 @@ public class AppointmentService {
     }
 
     public Appointment update(Long id, Appointment appointmentInput){
+        logger.info("Public method 'update' method called at "+ AppointmentService.class +" at line#" + lineGetter);
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (appointmentInput == null)return null;
+        logger.error("if statement from public Appointment 'update' inside "+ AppointmentService.class +" at line#" +
+                lineGetter + "- Error : 'appointmentInput is null");
         appointmentInput.setId(id);
         appointmentInput.setUpdatedAt(LocalDateTime.now());
         appointmentInput.setUpdatedBy(user);
@@ -75,8 +84,11 @@ public class AppointmentService {
 
     public boolean canEdit(Long id) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        logger.info("Public method 'canEdit' method called at "+ AppointmentService.class +" at line#" + lineGetter);
         Optional<Appointment> appointment = appointmentRepository.findById(id);
         if(!appointment.isPresent())return false;
+        logger.error("if statement from public Appointment 'update' inside "+ AppointmentService.class +" at line#" +
+                lineGetter + "- Error : 'appointmentInput is not present");
         return appointment.get().getCreatedBy().getId() == user.getId();
     }
 }
