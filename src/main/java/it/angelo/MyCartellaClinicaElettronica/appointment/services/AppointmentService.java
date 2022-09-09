ca0c9115d23573dc3b368074640ffc7d40df6e19
Angelo Zammataro <juba.kem@gmail.com>
2022-09-09 15:08:12 +0200

    Swagger Documentation

--- src/main/java/it/angelo/MyCartellaClinicaElettronica/appointment/services/AppointmentService.java
+++ src/main/java/it/angelo/MyCartellaClinicaElettronica/appointment/services/AppointmentService.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.text.DateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
