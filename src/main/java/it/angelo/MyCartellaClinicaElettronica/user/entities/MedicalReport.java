440eec63310241d7a8273bd7f495fbce8d389615
Mattia <oleandromattia@gmail.com>
2022-09-11 20:00:40 +0200

    Added swagger documentation(ApiModelProperty) to the "MedicalReport" class.

--- src/main/java/it/angelo/MyCartellaClinicaElettronica/user/entities/MedicalReport.java
+++ src/main/java/it/angelo/MyCartellaClinicaElettronica/user/entities/MedicalReport.java
@@ -2,6 +2,7 @@ package it.angelo.MyCartellaClinicaElettronica.user.entities;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import io.swagger.annotations.ApiModelProperty;
 import it.angelo.MyCartellaClinicaElettronica.utils.entities.BaseEntity;
 import lombok.Data;
 
@@ -17,17 +18,30 @@ public class MedicalReport extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @ApiModelProperty(value = "The id of the medical report", example = "01")
     private Long id;
+
+    @ApiModelProperty(value = "The anamnesis of the medical report", example = " ")//TODO completare documentazione con esempio
     private String anamnesis;
+
+    @ApiModelProperty(value = "The diagnosis of the medical report", example = " ")//TODO completare documentazione con esempio
     private String diagnosis;
+
+    @ApiModelProperty(value = "The therapy of the medical report", example = "Physical therapy")
     private String therapy;
+
+    @ApiModelProperty(value = "The prognosis of the medical report", example = " ")//TODO completare documentazione con esempio
     private String prognosis;
+
+    @ApiModelProperty(value = "The medical examination of the medical report", example = " ")//TODO completare documentazione con esempio
     private String medicalExamination;
 
     @OneToOne
+    @ApiModelProperty(value = " ", example = " ")//TODO chiedere ad Angelo
     private User patient;
 
     @ManyToOne
+    @ApiModelProperty(value = " ", example = " ")//TODO chiedere ad Angelo
     private User doctor;
 
     //proprietario della relazione
@@ -35,6 +49,7 @@ public class MedicalReport extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "medical_record_id")
     @JsonManagedReference //necessario per evitare Infinite recursion (StackOverflowError)
+    @ApiModelProperty(value = " ", example = " ")//TODO chiedere ad Angelo
     private MedicalRecord medicalRecord; // lo trovo in MedicalRecord mappedBy
 
 }
